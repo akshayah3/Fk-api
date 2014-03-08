767d05df522d11328eecb63f55c385fc344bdcfa
akshayah3 <akshaynukala95@gmail.com>
2014-03-08 16:19:23 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+__author__ = 'Akshay'
+
+class Flipkart:
+    global path = 'http://www.flipkart.com'
+
+    def __init__(self):
+        pass
+
+class mobiles:
+    mobiles = {'samsung': path + '/q/samsung-mobiles',
+               'micromax': path + '/q/micromax-mobiles',
+               'sony': path + '/q/sony-mobiles',
+               'htc': path + '/q/htc-mobiles',
+               'apple': path + '/q/apple-mobiles',
+               'blackberry': path + '/q/blackberry-mobiles',
+               'lg': path + '/q/lg-mobiles',
+               'karbonn': path + '/q/karbonn-mobiles',
+               'maxx': path + '/q/maxx-mobiles'}
+    def __init__(self):
+        pass
