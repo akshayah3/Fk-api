076d1f32beac5e3fea37b7ffe6c0487acd2e9dd6
akshayah3 <akshaynukala95@gmail.com>
2014-03-13 17:06:36 +0530

    get_mobiles

--- main.py
+++ main.py
@@ -2,24 +2,28 @@ __author__ = 'Akshay'
 
 import BeautifulSoup as bs
 import requests
+path = 'http://www.flipkart.com'
+mobiles1 = ['samsung', 'nokia', 'apple', 'lg', 'micromax', 'maxx', 'htc', 'sony']
 
 class Flipkart:
-    global path = 'http://www.flipkart.com'
 
     def __init__(self):
         pass
 
 class mobiles:
-    mobiles = {'samsung': path + '/q/samsung-mobiles',
-               'micromax': path + '/q/micromax-mobiles',
-               'sony': path + '/q/sony-mobiles',
-               'htc': path + '/q/htc-mobiles',
-               'apple': path + '/q/apple-mobiles',
-               'blackberry': path + '/q/blackberry-mobiles',
-               'lg': path + '/q/lg-mobiles',
-               'karbonn': path + '/q/karbonn-mobiles',
-               'maxx': path + '/q/maxx-mobiles'}
+    mobiles1 = ['samsung', 'nokia', 'apple', 'lg', 'micromax', 'maxx', 'htc', 'sony']
+
     def __init__(self):
         pass
 
+    def get_mobiles(self, mobile, number = 5):
+        if mobile in mobiles1:
+            request = requests.get(path + '/mobiles/' + mobile +'~brand/pr?sid=tyy,4io&otracker=hp_nmenu_sub_electronics_0_' + mobile)
+            request = request.text
+            response = bs.BeautifulSoup(request)
+            a = response.findAll('div', {'class': 'pu-title fk-font-13'})
+            print [all(str(i.text) for i in a)]
 
+
+akshay = mobiles()
+akshay.get_mobiles('samsung')
